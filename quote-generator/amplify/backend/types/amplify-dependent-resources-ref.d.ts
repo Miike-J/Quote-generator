4176ea56a0821a799779987cdf640cd91513c4ce
Miike-J <michaeljones98@hotmail.co.uk>
2023-06-09 16:58:28 +0100

    add api + lambda + auth

---
+++ quote-generator/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quotegenerator": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "quotegenerator86cb3df7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "inspoquoteLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- quote-generator/pages/index.tsx
+++ quote-generator/pages/index.tsx
@@ -1,10 +1,19 @@
+import React, {useState} from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgourndCon } from '@/components/QuoteGenerator/QuoteGeneratorElements';
+
+// Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle } from '@/components/QuoteGenerator/QuoteGeneratorElements';
+
+//Assests
+import Cloud1 from '@/assests/Cloud1.png'
+import Cloud2 from '@/assests/Cloud2.png'
 
 
 export default function Home() {
+  const [numberOfQuotes, setNumberofQuotes] = useState<Number | null>(0);
   return (
     <>
       <Head>
@@ -14,9 +23,61 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/*Background*/}
-      <GradientBackgourndCon>
-        
-      </GradientBackgourndCon>
+      <GradientBackgroundCon>
+
+        {/* Quote Generator Modal Pop-up */}
+        {/* <QuoteGeneratorModal
+        /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io" target="blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+
+
+
+
+      {/*Background images */}  
+      <BackgroundImage1
+        src={Cloud1}
+        height="300"
+        alt="cloudybackground1"
+        />
+
+      <BackgroundImage2
+        src={Cloud2}
+        height="300"
+        alt="cloudybackground2"
+        />
+
+
+        {/* Footer */}
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed by <FooterLink href="https://github.com/Miike-J" target="_blank" rel="noopener noreferrer"> Miike-J
+            </FooterLink>
+          </>
+        </FooterCon>
+
+      </GradientBackgroundCon>
     </>
   )
 }
